2555e7dd6bd2f45ae71c8563f5f09fa26c3ab41b
Ana <ana24garcia07@gmail.com>
2024-11-26 10:29:22 +0100

    Backup

---
+++ 03-poo-dom/Relacion.md
@@ -0,0 +1,2 @@
+Implementar el uso de POO, un sistema para iniciar sesión y actualizar un perfil de un usuario
+--> el objetivo será crear una clase/objeto para modelar el inicio de sesión/perfil
---
+++ 03-poo-dom/src/main.js
@@ -0,0 +1,6 @@
+import { Usuario } from "./components/Usuario/Usuario";
+
+// Crear instancia de Usuario
+const Usuario = new Usuario("ana", "xx@ejemplo.com", "1234");
+
+const app = document.getElementById("app");
